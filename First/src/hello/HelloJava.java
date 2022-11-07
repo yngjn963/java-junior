bebbb3d8bbb938bd3c00368be071cc0be4b1660c
yngjn963 <yngjn963@naver.com>
2022-11-07 19:27:19 +0900

    test: eclipse and git integration. test commit and push.

---
+++ First/src/hello/HelloJava.java
@@ -0,0 +1,15 @@
+/*
+ * Date : 2022년 11월 4일
+ * Author : 김용진
+ * Description : 첫 번째 자바 프로그램입니다.
+ * Version : 1.0
+ */
+package hello;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		//System.out.println("Hello, Java");
+	}
+
+}
