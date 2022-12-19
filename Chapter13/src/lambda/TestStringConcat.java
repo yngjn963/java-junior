d3c7269b2a621fd5cc1afd1b9d24c5f3598bc83a
yngjn963 <yngjn963@naver.com>
2022-12-19 23:12:37 +0900

    feat: Do it! 자바 프로그래밍 입문 예제 코딩
    람다식

---
+++ Chapter13/src/lambda/TestStringConcat.java
@@ -0,0 +1,12 @@
+package lambda;
+
+public class TestStringConcat {
+
+	public static void main(String[] args) {
+		String s1 = "Hello";
+		String s2 = "World";
+		StringConcatImpl concat1 = new StringConcatImpl();
+		concat1.makeString(s1, s2);
+	}
+
+}
