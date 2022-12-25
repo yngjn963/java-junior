5de1d5cc80928b96b64e12a18e2f3df8136c0fc5
yngjn963 <yngjn963@naver.com>
2022-12-25 22:30:03 +0900

    feat: Do it! 자바 프로그래밍 입문 예제 코딩
    람다식

--- Chapter13/src/lambda/TestStringConcat.java
+++ Chapter13/src/lambda/TestStringConcat.java
@@ -13,6 +13,9 @@ public class TestStringConcat {
 		
 		StringConcat concat3 = (s, v) -> {
 			//i = 200; //람다식 내부에서 변경하면 오류 발생
+			//지역 변수는 메서드 호출이 끝나면 메모리에서 사라지기 때문에 익명 내부 클래스에서 사용하는 경우에는 지역 변수가 상수로 변한다.
+			//람다식 역시 익명 내부 클래스가 생성되므로 외부 메서드의 지역 변수를 사용하면 변수는 final 변수, 즉 상수가 된다.
+			//따라서 이 변수를 변경하면 오류가 발생한다.
 			System.out.println(i);
 			System.out.println(s + "," + v);
 		};
