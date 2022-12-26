a28f5ec19d3f43cc96ff711e942f42bac4b78fbf
yngjn963 <yngjn963@naver.com>
2022-12-26 23:25:17 +0900

    feat: Do it! 자바 프로그래밍 입문 예제 코딩
    람다식

--- Chapter13/src/lambda/MyNumber.java
+++ Chapter13/src/lambda/MyNumber.java
@@ -1,5 +1,9 @@
 package lambda;
 
+/*
+ * 자바에서 제공한느 함수형 프로그래밍 방식을 '람다식(Lambda expression)'이라고 한다.
+ */
+
 public interface MyNumber {
 	int getMax(int num1, int num2);
 	int add(int num1, int num2);
