27606627491fc5e09e2afe23a69e93e61ee19248
yngjn963 <yngjn963@naver.com>
2023-02-14 22:59:51 +0900

    feat: Do it! 자바 프로그래밍 입문 예제 코딩
    바이트 단위 스트림

---
+++ Chapter15/src/stream/outputstream/FileOutputStreamTest.java
@@ -0,0 +1,19 @@
+package stream.outputstream;
+
+/*
+ * OutputStream
+ * 바이트 단위로 쓰는 스트림 중 최상위 스트림
+ * 	    스트림 클래스						설명
+ * FileOutputStream			바이트 단위로 파일에 자료를 쓴다.
+ * ByteArrayOutputStream	Byte 배열에 바이트 단위로 자료를 쓴다.
+ * FilterOutputStream		기반 스트림에서 자료를 쓸 때 추가 기능을 제공하는 보조 스트림의 상위 클래스이다.
+ */
+
+public class FileOutputStreamTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
