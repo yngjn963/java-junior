5ce1a88356a95ccd065c5b43100cf4d39c210167
yngjn963 <yngjn963@naver.com>
2023-03-03 22:56:53 +0900

    feat: Do it! 자바 프로그래밍 입문 예제 코딩
    직렬화

---
+++ Chapter15/src/stream/serialization/SerializationTest.java
@@ -0,0 +1,27 @@
+package stream.serialization;
+
+class Person {
+	private static final long serialVersionUID = -1503252402544036183L;
+	String name;
+	String job;
+	
+	public Person() {}
+	
+	public Person(String name, String job) {
+		this.name = name;
+		this.job = job;
+	}
+	
+	public String toString() {
+		return name + "," + job;
+	}
+}
+
+public class SerializationTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
